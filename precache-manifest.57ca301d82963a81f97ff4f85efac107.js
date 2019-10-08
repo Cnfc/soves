50f549937b467fb4df02ca21f4a7c064ef6d6ae7
Cnfc <stdashkov@mail.ru>
2019-10-08 15:28:37 +0300

    Updates

---
+++ precache-manifest.57ca301d82963a81f97ff4f85efac107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f57b4662a804f8490b316ccd700721a",
+    "url": "/soves/index.html"
+  },
+  {
+    "revision": "4d62bca87e9a6c261bb1",
+    "url": "/soves/static/css/main.a22c9842.chunk.css"
+  },
+  {
+    "revision": "18fa5f10f1f6cc837ee2",
+    "url": "/soves/static/js/2.9884ce95.chunk.js"
+  },
+  {
+    "revision": "4d62bca87e9a6c261bb1",
+    "url": "/soves/static/js/main.bd0e776f.chunk.js"
+  },
+  {
+    "revision": "1b3f2b6489656ce460d5",
+    "url": "/soves/static/js/runtime-main.eb53dc8e.js"
+  }
+]);
